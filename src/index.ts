614c7434b6047363d140af03818a09e981271936
Fernando Silva <fernandonsilva16@gmail.com>
2023-02-05 10:18:43 -0300

    feat(server): add koa with graphql

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-// eslint-disable-next-line no-console
-console.log('Hello World!');
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+
+import { routes } from './routes';
+
+const app = new Koa();
+
+app.use(
+  bodyParser({
+    onerror(err, ctx) {
+      ctx.throw(err, 422);
+    },
+  }),
+);
+
+app.use(routes.routes()).use(routes.allowedMethods());
+
+app.listen(5001, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server listening to port 5001');
+});
